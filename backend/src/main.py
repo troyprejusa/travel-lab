63f61ea8d9d710ab1591c52cf2469d0d1acd242b
Troy Prejusa <troyprejusa@icloud.com>
2023-06-07 21:56:08 -0700

    Able to connect to database, but unable to create new tables with psycopg at this time

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from routers.project import project_router
-# from models.db_connect import cursor
+from models.db_connect import cursor
+
+try:
+    cursor.execute("CREATE TABLE IF NOT EXISTS python;")
+    cursor.commit()
+except Exception:
+    print(str(Exception))
+    print('Unable to create table')
 
 app = FastAPI()
 
