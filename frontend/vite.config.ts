d9224c0760ef17680431efacbff9579df8b81a99
troyprejusa <troyprejusa@gmail.com>
2023-08-01 21:29:16 -0700

    Add proxy configuration for vite dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const backend_server: string = 'http://localhost:8000';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/dev': backend_server,
+      '/auth': backend_server,
+      '/user': backend_server,
+      '/trip': backend_server
+    }
+  }
 })
