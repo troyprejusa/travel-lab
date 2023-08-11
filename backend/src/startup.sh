3abb48ac4edff9ec562a40a68fb63e99bb62659c
troyprejusa <troyprejusa@gmail.com>
2023-08-10 21:06:05 -0700

    Add debug configurations to support remote debugging from outside container

--- backend/src/startup.sh
+++ backend/src/startup.sh
@@ -5,4 +5,9 @@ pip install --quiet -r /app/requirements.txt
 
 # Start the FastAPI app with Uvicorn
 cd /app/src     # Navigate to the main module directory
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-level warning
+
+# With debug server
+python -m debugpy --listen 0.0.0.0:5678 -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-level warning
+
+# No debugger
+# uvicorn main:app --host 0.0.0.0 --port 8000 --reload --log-level warning
