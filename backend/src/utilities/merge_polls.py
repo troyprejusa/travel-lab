d338d6ac526d25de54aaf70a5fa816a7925dde88
troyprejusa <troyprejusa@gmail.com>
2023-08-20 08:19:27 -0700

    Save cleaned up logic for poll merging, reducing duplicate calls to shared functions

--- backend/src/utilities/merge_polls.py
+++ backend/src/utilities/merge_polls.py
@@ -3,8 +3,9 @@ from models.Schemas import PollResponseBody, PollVoteBody
 
 def merge_polls(data: list[dict]) -> list[dict]:
     # Note that the below logic relies on the data being sorted 
-    # by poll_id. This is basically a merge intervals problem.
-
+    # by poll_id from the query. After that, this is basically
+    # the merge intervals problem
+    #
     # Per FastAPI, can either return actual constructions of 
     # the various classes or we can return dictionaries. One
     # problem is that with the BaseModel schemas, you must
@@ -20,15 +21,14 @@ def merge_polls(data: list[dict]) -> list[dict]:
     for i in range(1, len(data)):
         row = data[i]
         if row['poll_id'] != poll_body['poll_id']:
-            # Save this to the output
+            # Save the previous poll data to the output
             output.append(poll_body)
 
-            # Start a new object
+            # Create a new poll object
             poll_body = create_new_poll(row)
         
-        else:
-            # Append the option to the right place
-            add_option_to_data(row, poll_body)
+        # Append this vote to the right place
+        add_vote_to_poll(row, poll_body)
     
     # Add the last remaining item to the output
     output.append(poll_body)
@@ -43,16 +43,14 @@ def create_new_poll(row: dict) -> dict:
     poll_body['anonymous'] = row['anonymous']
     poll_body['options'] = []
 
-    new_vote = create_new_vote(row)
-
-    append_possible_anon(new_vote, poll_body['anonymous'], row['voted_by'])
+    new_vote = create_new_option(row)
 
     # Add the vote option into the poll object
     poll_body['options'].append(new_vote)
 
     return poll_body
 
-def create_new_vote(row: dict) -> dict:
+def create_new_option(row: dict) -> dict:
     new_vote = {}
     new_vote['option_id'] = row['option_id']
     new_vote['option'] = row['option']
@@ -61,19 +59,20 @@ def create_new_vote(row: dict) -> dict:
     return new_vote
 
 
-def add_option_to_data(row: dict, poll_body: dict) -> None:
+def add_vote_to_poll(row: dict, poll_body: dict) -> None:
+    # Check if this option already exists in the options
     for option in poll_body['options']:
         if option['option_id'] == row['option_id']:
-            append_possible_anon(option, poll_body['anonymous'], row['voted_by'])
+            append_vote(option, poll_body['anonymous'], row['voted_by'])
             return
         
-    # If we got to here, we didn't find a matching option. Add a new one.
-    new_vote = create_new_vote(row)
-    append_possible_anon(new_vote, poll_body['anonymous'], row['voted_by'])
+    # If we got to here, we didn't find an existing option. Add a new one.
+    new_vote = create_new_option(row)
+    append_vote(new_vote, poll_body['anonymous'], row['voted_by'])
     poll_body['options'].append(new_vote)
         
 
-def append_possible_anon(poll_vote: dict, anonymous: bool, voter: str) -> None:
+def append_vote(poll_vote: dict, anonymous: bool, voter: str) -> None:
     if voter is None:
         return
     
