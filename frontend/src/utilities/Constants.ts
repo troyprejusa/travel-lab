f3ed5ae22a9354269ce02943441a15a7a3ab1316
troyprejusa <troyprejusa@gmail.com>
2023-09-06 09:20:43 -0700

    Commit missed file for previous commit

--- frontend/src/utilities/Constants.ts
+++ frontend/src/utilities/Constants.ts
@@ -2,8 +2,11 @@ const Constants = {
   COLORS: ['#00C49F', '#FFBB28', '#0088FE', '#FF8042', '#9933FF'],
 
   AUTH0_DOMAIN: import.meta.env.VITE_AUTH0_DOMAIN,
-  AUTH0_CLIENT:import.meta.env.VITE_AUTH0_CLIENT,
-  AUTH0_AUDIENCE: import.meta.env.VITE_AUTH0_AUDIENCE
+  AUTH0_CLIENT: import.meta.env.VITE_AUTH0_CLIENT,
+  AUTH0_AUDIENCE: import.meta.env.VITE_AUTH0_AUDIENCE,
+
+  API_HOST: import.meta.env.VITE_API_HOST,
+  API_PORT: import.meta.env.VITE_API_PORT
 };
 
 export default Constants;
