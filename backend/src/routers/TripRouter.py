81756c99ee00d294ce1db21aa660daa088a9dd61
troyprejusa <troyprejusa@gmail.com>
2023-09-30 21:44:58 -0700

    Removed deprecated markings from REST calls in TripRouter

--- backend/src/routers/TripRouter.py
+++ backend/src/routers/TripRouter.py
@@ -13,7 +13,8 @@ trip_router = APIRouter(
     prefix='/trip'
 )
 
-# Create a trip
+# --------------- TRIP OPERATIONS --------------- #
+
 @trip_router.post('/')
 async def create_trip(
     request: Request,
@@ -53,7 +54,6 @@ async def create_trip(
             }
         )
 
-# Delete a trip
 @trip_router.delete('/{trip_id}')
 async def delete_trip(request: Request, trip_id: str) -> dict[str, str]:
     try:
@@ -78,9 +78,9 @@ async def delete_trip(request: Request, trip_id: str) -> dict[str, str]:
                 "message": f"ERROR: Unable to delete trip {trip_id}"
             }
         )
+    
+# --------------- ITINERARY OPERATIONS --------------- #
 
-
-# Get itinerary entries for this trip
 @trip_router.get('/{trip_id}/itinerary')
 async def get_itinerary_info(request: Request, trip_id: str) -> list[ItineraryModel] | str:
     try:
@@ -100,27 +100,70 @@ async def get_itinerary_info(request: Request, trip_id: str) -> list[ItineraryMo
                 "message": f"ERROR: Unable to retrieve itinerary for trip id {trip_id}"
             }
         )
+    
+@trip_router.post('/{trip_id}/itinerary')
+async def add_itinerary_stop(
+    request: Request,
+    trip_id: str,
+    title: Annotated[str, Form()],
+    start_time: Annotated[datetime, Form()],
+    end_time: Annotated[datetime, Form()],
+    description: Annotated[str | None, Form()] = None
+    ) ->  dict[str, str]:
 
-@trip_router.get('/{trip_id}/message')
-async def get_messages(request: Request, trip_id: str) -> list[MessageModel] | str:
     try:
         verify_attendance(trip_id, request.state.user['trips'])
 
-        data = db_handler.query("""
-            SELECT * FROM message WHERE trip_id = %s ORDER BY id;
-        """, (trip_id,))
-
-        return data
+        db_handler.query("""
+        INSERT INTO itinerary (trip_id, title, description, start_time, end_time, created_by)
+        VALUES (%s, %s, %s, %s, %s, %s);
+        """, (trip_id, title, description, start_time, end_time, request.state.user['email']))
+     
+        return JSONResponse(
+            status_code=200,
+            content= {
+                "message": "SUCCESS: Created itinerary stop"
+            }
+        )
     
     except Exception as error:
         print(error)
         return JSONResponse(
             status_code=500,
-            content= {
-                "message": f'ERROR: Unable to retrieve messages for trip id {trip_id}'
+            content = {
+                "message": f"ERROR: Unable to submit itinerary stop for trip id {trip_id}"
             }
         )
     
+@trip_router.delete('/{trip_id}/itinerary/{item_id}')
+async def remove_itinerary_stop(request: Request, trip_id: str, item_id: int) ->  str:
+    try:
+        verify_admin(trip_id, request.state.user['trips'])
+
+        # Even though we technically only need the item id to delete, we
+        # will also use the trip id for improved robustness since we have it
+        db_handler.query("""
+            DELETE FROM itinerary WHERE trip_id=%s AND id=%s;
+        """, (trip_id, item_id))
+     
+        return JSONResponse(
+            status_code=200,
+            content= {
+                "message": f"SUCCESS: Deleted itinerary stop from trip id {trip_id}"
+            }
+        )
+    
+    except Exception as error:
+        print(error)
+        return JSONResponse(
+            status_code=500,
+            content = {
+                "message": f"ERROR: Unable to delete itinerary stop for trip id {trip_id}"
+            }
+        )
+    
+# --------------- POLL OPERATIONS --------------- #
+
 @trip_router.get('/{trip_id}/poll')
 async def get_polls(request: Request, trip_id: str) -> list[PollResponse] | str:
     try:
@@ -169,122 +212,10 @@ async def get_polls(request: Request, trip_id: str) -> list[PollResponse] | str:
                 "message": f'ERROR: Unable to retrieve polls for trip id {trip_id}'
             }
         )
-
-@trip_router.get('/{trip_id}/packing')
-async def get_packing_items(request: Request, trip_id: str) -> list[PackingModel] | dict[str, str]:
-    try:
-        verify_attendance(trip_id, request.state.user['trips'])
-
-        data = db_handler.query("""
-            SELECT * FROM packing WHERE trip_id=%s ORDER BY id;
-        """, (trip_id,))
-
-        return data
-
-    except Exception as error:
-        return JSONResponse(
-            status_code=500,
-            content= {
-                "message": f'Unable to retrieve itms for trip {trip_id}'
-            }
-        )
     
-# Get info for travellers on this trip
-@trip_router.get('/{trip_id}/travellers')
-async def get_travellers(request: Request, trip_id: str) ->  list[TravellerResponse] | str:
-    try:
-        verify_attendance(trip_id, request.state.user['trips'])
-        
-        travellers = db_handler.query("""
-            SELECT traveller.*, traveller_trip.confirmed, traveller_trip.admin
-            FROM traveller 
-            JOIN traveller_trip ON traveller.id = traveller_trip.traveller_id
-            WHERE traveller_trip.trip_id = %s
-            ORDER BY last_name;
-            """, (trip_id,))
-        
-        return travellers
 
-    except Exception as error:
-        print(error)
-        return JSONResponse(
-            status_code=500,
-            content = {
-                "message": f"ERROR: Unable to find travellers for trip id {trip_id}"
-            }
-        )
-
-# ---------------------- DEPRECATED REST CALLS ---------------------- #
-
-# @DEPRECATED: Moved to websockets
-# Add itinerary stop
-@trip_router.post('/{trip_id}/itinerary')
-async def add_itinerary_stop(
-    request: Request,
-    trip_id: str,
-    title: Annotated[str, Form()],
-    start_time: Annotated[datetime, Form()],
-    end_time: Annotated[datetime, Form()],
-    description: Annotated[str | None, Form()] = None
-    ) ->  dict[str, str]:
-
-    try:
-        verify_attendance(trip_id, request.state.user['trips'])
-
-        db_handler.query("""
-        INSERT INTO itinerary (trip_id, title, description, start_time, end_time, created_by)
-        VALUES (%s, %s, %s, %s, %s, %s);
-        """, (trip_id, title, description, start_time, end_time, request.state.user['email']))
-     
-        return JSONResponse(
-            status_code=200,
-            content= {
-                "message": "SUCCESS: Created itinerary stop"
-            }
-        )
-    
-    except Exception as error:
-        print(error)
-        return JSONResponse(
-            status_code=500,
-            content = {
-                "message": f"ERROR: Unable to submit itinerary stop for trip id {trip_id}"
-            }
-        )
-
-# @DEPRECATED: Moved to websockets
-# Remove itinerary stop
-@trip_router.delete('/{trip_id}/itinerary/{item_id}')
-async def remove_itinerary_stop(request: Request, trip_id: str, item_id: int) ->  str:
-    try:
-        verify_admin(trip_id, request.state.user['trips'])
-
-        # Even though we technically only need the item id to delete, we
-        # will also use the trip id for improved robustness since we have it
-        db_handler.query("""
-            DELETE FROM itinerary WHERE trip_id=%s AND id=%s;
-        """, (trip_id, item_id))
-     
-        return JSONResponse(
-            status_code=200,
-            content= {
-                "message": f"SUCCESS: Deleted itinerary stop from trip id {trip_id}"
-            }
-        )
-    
-    except Exception as error:
-        print(error)
-        return JSONResponse(
-            status_code=500,
-            content = {
-                "message": f"ERROR: Unable to delete itinerary stop for trip id {trip_id}"
-            }
-        )
-
-# @DEPRECATED: Moved to websockets
-# Create poll
 @trip_router.post('/{trip_id}/poll')
-async def add_poll(request: Request, trip_id: str, poll_body: PollRequestWS) -> dict[str, str]:
+async def create_poll(request: Request, trip_id: str, poll_body: PollRequestWS) -> dict[str, str]:
     try:
         verify_attendance(trip_id, request.state.user['trips'])
 
@@ -319,8 +250,6 @@ async def add_poll(request: Request, trip_id: str, poll_body: PollRequestWS) -> 
             }
         )
 
-# @DEPRECATED: Moved to websockets
-# Delete poll
 @trip_router.delete('/{trip_id}/poll/{poll_id}')
 async def delete_poll(request: Request, trip_id: str, poll_id: int) -> dict[str, str]:
     try:
@@ -348,8 +277,27 @@ async def delete_poll(request: Request, trip_id: str, poll_id: int) -> dict[str,
             }
         )
 
-# @DEPRECATED: Moved to websockets
-# Add packing item
+# --------------- PACKING OPERATIONS --------------- #
+
+@trip_router.get('/{trip_id}/packing')
+async def get_packing_items(request: Request, trip_id: str) -> list[PackingModel] | dict[str, str]:
+    try:
+        verify_attendance(trip_id, request.state.user['trips'])
+
+        data = db_handler.query("""
+            SELECT * FROM packing WHERE trip_id=%s ORDER BY id;
+        """, (trip_id,))
+
+        return data
+
+    except Exception as error:
+        return JSONResponse(
+            status_code=500,
+            content= {
+                "message": f'Unable to retrieve itms for trip {trip_id}'
+            }
+        )
+    
 @trip_router.post('/{trip_id}/packing')
 async def add_packing_item(
     request: Request,
@@ -383,8 +331,57 @@ async def add_packing_item(
             }
         )
 
-# @DEPRECATED: Moved to websockets
-# Delete packing item
+@trip_router.patch('/{trip_id}/packing/claim/{item_id}')
+async def claim_packing_item(request: Request, trip_id: str, item_id: int) -> dict[str, str]:
+    try:
+        verify_attendance(trip_id, request.state.user['trips'])
+
+        # Allow a user to claim this item as long as it is not currently claimed
+        db_handler.query("""
+            UPDATE packing SET packed_by = %s WHERE packed_by IS NULL AND id = %s;
+        """, (request.state.user['email'], item_id))
+
+        return JSONResponse(
+            status_code=200,
+            content= {
+                "message": f'Successfully cliaimed item_id {item_id} on trip {trip_id}'
+            }
+        )
+
+    except Exception as error:
+        print(error)
+        return JSONResponse(
+            status_code=500,
+            content= {
+                "message": f'Unable to claim item_id {item_id}'
+            }
+        )
+    
+@trip_router.patch('/{trip_id}/packing/unclaim/{item_id}')
+async def unclaim_packing_item(request: Request, trip_id: str, item_id: int) -> dict[str, str]:
+    try:
+        verify_attendance(trip_id, request.state.user['trips'])
+
+        db_handler.query("""
+            UPDATE packing SET packed_by = NULL WHERE packed_by IS NOT NULL AND id = %s;
+        """, (item_id,))
+
+        return JSONResponse(
+            status_code=200,
+            content= {
+                "message": f'Successfully unclaimed item_id {item_id} on trip {trip_id}'
+            }
+        )
+
+    except Exception as error:
+        print(error)
+        return JSONResponse(
+            status_code=500,
+            content= {
+                "message": f'Unable to unclaim item_id {item_id}'
+            }
+        )
+    
 @trip_router.delete('/{trip_id}/packing/{item_id}')
 async def delete_packing_item(request: Request, trip_id: str, item_id: int) -> dict[str, str]:
     try:
@@ -411,59 +408,51 @@ async def delete_packing_item(request: Request, trip_id: str, item_id: int) -> d
                 "message": f'Unable to delete item_id {item_id} from trip {trip_id}'
             }
         )
+    
+# --------------- MESSAGE OPERATIONS --------------- #
 
-# @DEPRECATED: Moved to websockets
-# Claim packing item
-@trip_router.patch('/{trip_id}/packing/claim/{item_id}')
-async def claim_packing_item(request: Request, trip_id: str, item_id: int) -> dict[str, str]:
+@trip_router.get('/{trip_id}/message')
+async def get_messages(request: Request, trip_id: str) -> list[MessageModel] | str:
     try:
         verify_attendance(trip_id, request.state.user['trips'])
 
-        # Allow a user to claim this item as long as it is not currently claimed
-        db_handler.query("""
-            UPDATE packing SET packed_by = %s WHERE packed_by IS NULL AND id = %s;
-        """, (request.state.user['email'], item_id))
+        data = db_handler.query("""
+            SELECT * FROM message WHERE trip_id = %s ORDER BY id;
+        """, (trip_id,))
 
-        return JSONResponse(
-            status_code=200,
-            content= {
-                "message": f'Successfully cliaimed item_id {item_id} on trip {trip_id}'
-            }
-        )
-
+        return data
+    
     except Exception as error:
         print(error)
         return JSONResponse(
             status_code=500,
             content= {
-                "message": f'Unable to claim item_id {item_id}'
+                "message": f'ERROR: Unable to retrieve messages for trip id {trip_id}'
             }
         )
+    
+# --------------- TRAVELLER OPERATIONS --------------- #
 
-# @DEPRECATED: Moved to websockets
-# Unclaim packing item
-@trip_router.patch('/{trip_id}/packing/unclaim/{item_id}')
-async def unclaim_packing_item(request: Request, trip_id: str, item_id: int) -> dict[str, str]:
+@trip_router.get('/{trip_id}/travellers')
+async def get_travellers(request: Request, trip_id: str) ->  list[TravellerResponse] | str:
     try:
         verify_attendance(trip_id, request.state.user['trips'])
-
-        db_handler.query("""
-            UPDATE packing SET packed_by = NULL WHERE packed_by IS NOT NULL AND id = %s;
-        """, (item_id,))
-
-        return JSONResponse(
-            status_code=200,
-            content= {
-                "message": f'Successfully unclaimed item_id {item_id} on trip {trip_id}'
-            }
-        )
+        
+        travellers = db_handler.query("""
+            SELECT traveller.*, traveller_trip.confirmed, traveller_trip.admin
+            FROM traveller 
+            JOIN traveller_trip ON traveller.id = traveller_trip.traveller_id
+            WHERE traveller_trip.trip_id = %s
+            ORDER BY last_name;
+            """, (trip_id,))
+        
+        return travellers
 
     except Exception as error:
         print(error)
         return JSONResponse(
             status_code=500,
-            content= {
-                "message": f'Unable to unclaim item_id {item_id}'
+            content = {
+                "message": f"ERROR: Unable to find travellers for trip id {trip_id}"
             }
         )
-
