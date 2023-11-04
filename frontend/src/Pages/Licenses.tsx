a300ed0bbdbfed737e445553fe3bbcef4de53aa3
troyprejusa <troyprejusa@gmail.com>
2023-11-04 11:46:44 -0700

    Add placeholder strings to the licenses and terms files

--- frontend/src/Pages/Licenses.tsx
+++ frontend/src/Pages/Licenses.tsx
@@ -1,9 +1,13 @@
 import TitleBar from "../Components/TitleBar";
+import { Text } from "@chakra-ui/react";
 
 function Licenses() {
   return (
     <>
       <TitleBar text="Third Party Licenses"/>
+      <Text>
+        $license-info
+      </Text>
     </>
   );
 }
--- frontend/src/Pages/TermsAndConditions.tsx
+++ frontend/src/Pages/TermsAndConditions.tsx
@@ -1,9 +1,13 @@
+import { Text } from "@chakra-ui/react";
 import TitleBar from "../Components/TitleBar";
 
 function TermsAndConditions() {
     return (
       <>
         <TitleBar text="Terms and Conditions"/>
+        <Text>
+          $terms-and-conditions
+        </Text>
       </>
     );
   }
