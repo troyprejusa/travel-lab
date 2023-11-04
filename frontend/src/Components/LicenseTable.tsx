7effa20304ec0cc6f5da1f824fe6180b6d392682
troyprejusa <troyprejusa@gmail.com>
2023-11-04 13:05:19 -0700

    Add license information to frontend

---
+++ frontend/src/Components/LicenseTable.tsx
@@ -0,0 +1,47 @@
+import { LicenseInfo } from '../Pages/Licenses';
+import {
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from '@chakra-ui/react';
+
+
+interface LicenseTableProps {
+  data: Array<LicenseInfo>
+}
+function LicenseTable(props: LicenseTableProps) {
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Package</Th>
+            <Th>Repository</Th>
+            <Th>Copyright</Th>
+            <Th>License(s)</Th>
+            <Th>License Content</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {props.data.map((datum: LicenseInfo) => (
+            <Tr>
+              <Td>{datum.package}</Td>
+              <Td>{datum.repository}</Td>
+              <Td>{datum.copyright}</Td>
+              <Td>{datum.licenses}</Td>
+              <Td>{datum.licenseText}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default LicenseTable;
--- frontend/src/Pages/Licenses.tsx
+++ frontend/src/Pages/Licenses.tsx
@@ -1,15 +1,44 @@
-import TitleBar from "../Components/TitleBar";
-import { Text } from "@chakra-ui/react";
+import TitleBar from '../Components/TitleBar';
+import { useEffect, useState } from 'react';
+import LicenseTable from '../Components/LicenseTable';
+import { Text } from '@chakra-ui/react';
+
+export interface LicenseInfo {
+  package: string,
+  repository: string, 
+  copyright: string,
+  licenses: string,
+  licenseText: string
+}
 
 function Licenses() {
+  const [licenses, setLicenses] = useState<Array<LicenseInfo>>([]);
+  useEffect(() => {
+    getLicenseInfo();
+  }, []);
+
   return (
     <>
-      <TitleBar text="Third Party Licenses"/>
-      <Text>
-        $license-info
-      </Text>
+      <TitleBar text="Third Party Licenses" />
+      <Text>{licenses ? <LicenseTable data={licenses}/> : 'Fetching licenses...'}</Text>
     </>
   );
+
+  async function getLicenseInfo() {
+    try {
+      const res: Response = await fetch('/third-party-licenses.json');
+      if (res.ok) {
+        const data: any = await res.json();
+        const licenseData: Array<LicenseInfo> = [];
+        for (const key in data) {
+          licenseData.push({package: key, ...data[key]})
+        }
+        setLicenses(licenseData);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
 export default Licenses;
