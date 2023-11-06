628f5a0bcdacb26e7106d97344c52e7f266615b2
troyprejusa <troyprejusa@gmail.com>
2023-11-05 19:34:10 -0800

    Update licensing scripts
    add auth0 python

--- backend/src/scripts/license_info.sh
+++ backend/src/scripts/license_info.sh
@@ -4,7 +4,7 @@ original_dir=$(pwd)
 
 cd "$(dirname ${BASH_SOURCE})"
 cd ..
-pip-licenses --summary --from=mixed
+pip-licenses --summary --from=mixed > third_party_license_summary.txt
 pip-licenses --from=mixed --format=json --with-license-file --no-license-path --with-notice-file > third_party_licenses.json
 
 cd "$original_dir"
