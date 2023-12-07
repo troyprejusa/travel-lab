fe8fcc5c8846b86b7e13ccf4f66704be3679c5b3
troyprejusa <troyprejusa@gmail.com>
2023-12-06 16:04:19 -0800

    Comment out content in S3Handler for now

--- backend/src/models/S3Handler.py
+++ backend/src/models/S3Handler.py
@@ -2,16 +2,16 @@ from minio import Minio
 from minio.error import S3Error
 from utilities import Constants
 
-try:
-    print('S3Handler.py: Attempting to connect to file storage...')
-    minio_client = Minio(
-        f'{Constants.S3_HOST}:{Constants.S3_PORT}',
-        access_key=Constants.S3_ACCESS_KEY,
-        secret_key=Constants.S3_SECRET_KEY
-    )
-    print('S3Handler.py: Connected to file storage')
-except S3Error as e:
-    raise Exception('S3Handler.py: Unable to connect to file storage')
+# try:
+#     print('S3Handler.py: Attempting to connect to file storage...')
+#     minio_client = Minio(
+#         f'{Constants.S3_HOST}:{Constants.S3_PORT}',
+#         access_key=Constants.S3_ACCESS_KEY,
+#         secret_key=Constants.S3_SECRET_KEY
+#     )
+#     print('S3Handler.py: Connected to file storage')
+# except S3Error as e:
+#     raise Exception('S3Handler.py: Unable to connect to file storage')
 
 # if not minio_client.bucket_exists("user-photos"):
 #     minio_client.make_bucket("user-photos")
