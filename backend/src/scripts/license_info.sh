c4e1c4e379bfd1a45acfc6ba2be05c99f08f350d
troyprejusa <troyprejusa@gmail.com>
2023-12-17 10:07:45 -0800

    simplify backend scripts

--- backend/src/scripts/license_info.sh
+++ backend/src/scripts/license_info.sh
@@ -1,10 +1,6 @@
 #!usr/bin/bash
 
-original_dir=$(pwd)
-
-cd "$(dirname ${BASH_SOURCE})"
-cd ..
+echo "WARNING: You must execute this script from inside the container for it to work correctly!"
+cd /app/src
 pip-licenses --summary --from=mixed > third_party_license_summary.txt
 pip-licenses --from=mixed --format=json --with-license-file --no-license-path --with-notice-file > third_party_licenses.json
-
-cd "$original_dir"
--- backend/src/scripts/requirements_info.sh
+++ backend/src/scripts/requirements_info.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/bash
+
 echo "WARNING: You must execute this script from inside the container for it to work correctly!"
-
-original_dir=$(pwd)
-
-cd "$(dirname ${BASH_SOURCE})"
-cd ..
+cd /app/src
 pip freeze > requirements.txt
-
-cd "$original_dir"
