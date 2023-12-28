26a1ddd1cbd4379dd029ee50ead607de500b03a0
troyprejusa <troyprejusa@gmail.com>
2023-12-28 12:07:54 -0800

    Add email signature to received messages

--- frontend/src/Components/Messages.tsx
+++ frontend/src/Components/Messages.tsx
@@ -16,6 +16,7 @@ export const ReceivedMessage = (props: MessageProps) => {
     <Box textAlign={'left'} marginTop={'20px'} marginBottom={'20px'}>
       <Box {...messageDisplay} backgroundColor={Constants.BACKGROUND_TRANSPARENCY}>
         <Text>{props.content}</Text>
+        <Text float={'right'} fontSize={'xs'} color={'gray.600'}>{props.created_by}</Text>
       </Box>
     </Box>
   );
